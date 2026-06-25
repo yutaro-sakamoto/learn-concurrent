4e6394e8a2450fa9dd0953e06930f164893a4f0e
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-06-25 22:19:34 +0900

    wip: wip

--- cas.c
+++ cas.c
@@ -1,9 +1,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 bool compare_and_swap(uint64_t *p, uint64_t val, uint64_t newval) {
-    if (*p != val) {
-        return false;
-    }
-    *p = newval;
-    return true;
+    //if (*p != val) {
+    //    return false;
+    //}
+    //*p = newval;
+    //return true;
+    return __sync_bool_compare_and_swap(p, val, newval);
 }
