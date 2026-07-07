8c98faa7d7db688dccc16a9161d786af1ea97162
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-07-07 22:42:09 +0900

    wip: wip

---
+++ posix_semaphore.c
@@ -0,0 +1,30 @@
+#include <pthread.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define NUM_THREADS 10
+#define NUM_LOOP 10
+
+int count = 0;
+
+void* th(void* arg) {
+    sem_t *s = sem_open("/mysemaphore", 0);
+    if(s == SEM_FAILED) {
+        perror("sem_open");
+        exit(1);
+    }
+
+    for(int i=0; i<NUM_LOOP; i++) {
+        if(sem_wait(s) == -1) {
+            perror("sem_wait");
+            exit(1);
+        }
+    }
+
+    __sync_fetch_and_add(&count, 1);
+    printf("count = %d");
+}
