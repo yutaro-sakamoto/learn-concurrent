dcc097b6bb4201e0a3094e1fa0c0f76734bd8e7a
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-07-19 22:35:57 +0900

    wip: wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
 
 fn some_func(lock: Arc<Mutex<u64>>) {
@@ -9,18 +9,49 @@ fn some_func(lock: Arc<Mutex<u64>>) {
     }
 }
 
+fn child(id: u64, p: Arc<(Mutex<bool>, Condvar)>) {
+    let &(ref lock, ref cvar) = &*p;
+
+    let mut started = lock.lock().unwrap();
+    while !*started {
+        started = cvar.wait(started).unwrap();
+    }
+    //cvar.wait_while(started, |started| !*started).unwrap();
+    println!("child {}", id);
+}
+
+fn parent(p: Arc<(Mutex<bool>, Condvar)>) {
+    let &(ref lock, ref cvar) = &*p;
+    let mut started = lock.lock().unwrap();
+    *started = true;
+    cvar.notify_all();
+    println!("parent");
+}
+
 fn main() {
-    let lock0 = Arc::new(Mutex::new(0));
-    let lock1 = lock0.clone();
+    //let lock0 = Arc::new(Mutex::new(0));
+    //let lock1 = lock0.clone();
 
-    let th0 = thread::spawn(move || {
-        some_func(lock0);
-    });
+    //let th0 = thread::spawn(move || {
+    //    some_func(lock0);
+    //});
 
-    let th1 = thread::spawn(move || {
-        some_func(lock1);
-    });
+    //let th1 = thread::spawn(move || {
+    //    some_func(lock1);
+    //});
 
-    th0.join().unwrap();
-    th1.join().unwrap();
+    //th0.join().unwrap();
+    //th1.join().unwrap();
+
+    let pair0 = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair1 = pair0.clone();
+    let pair2 = pair0.clone();
+
+    let c0 = thread::spawn(move || child(0, pair0));
+    let c1 = thread::spawn(move || child(1, pair1));
+    let p = thread::spawn(move || parent(pair2));
+
+    c0.join().unwrap();
+    c1.join().unwrap();
+    p.join().unwrap();
 }
