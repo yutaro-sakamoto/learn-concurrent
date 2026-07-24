c3004193f87eaccf66518634aaeb2922627ad816
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-07-24 19:45:43 +0900

    wip: wip

---
+++ src/channel.rs
@@ -0,0 +1,31 @@
+use crate::semaphore::Semaphore;
+use std::collections::LinkedList;
+use std::sync::{Arc, Condvar, Mutex};
+
+#[derive(Clone)]
+pub struct Sender<T> {
+    sem: Arc<Semaphore>,
+    buf: Arc<Mutex<LinkedList<T>>>,
+    cond: Arc<Condvar>,
+}
+
+impl<T: Send> Sender<T> {
+    pub fn send(&self, data: T) {
+        self.sem.wait();
+        let mut buf = self.buf.lock().unwrap();
+        buf.push_back(data);
+        self.cond.notify_one();
+    }
+}
+
+pub struct Receiver<T> {
+    sem: Arc<Semaphore>,
+    buf: Arc<Mutex<LinkedList<T>>>,
+    cond: Arc<Condvar>,
+}
+
+//impl<T> Receiver<T> {
+//    pub fn recv(&self) -> T {
+//        let mut buf = self.
+//    }
+//}
